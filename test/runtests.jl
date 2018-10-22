bee0994ef775963007089cba5ac12d674f773496
RenanOD <renan_oclides@hotmail.com>
2018-10-22 16:01:32 -0200

    temporary add packages to run tests on travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
-#Pkg.add("JuMP")
-#Pkg.add("GLPK")
+Pkg.add("JuMP")
+Pkg.add("GLPK")
 
 using Simplex, Base.Test, JuMP, GLPKMathProgInterface
 
-include("test_simplexluup.jl")
-include("test_simplexinv.jl")
+  include("test_simplexluup.jl")
+  include("test_simplexinv.jl")
