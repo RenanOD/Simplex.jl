be4bdc28063e9b896dab0267a098088f7b192594
renanod <renan_oclides@hotmail.com>
2018-10-25 18:38:19 -0200

    stop running solvers twice

--- test/benchmarking.jl
+++ test/benchmarking.jl
@@ -4,21 +4,23 @@ using Base.Test, BenchmarkTools
 
 @testset "Testing benchmark problems" begin
   open("benchmark.log", "w") do logfile
-    for (m, n) in [(90,10) (450,50)]
-      for t = 1:2
+    for (m, n) in [(90,10) (450,50) (900,100)]
+      for t = 1:1
         # construct random transport problem given m and n
         A, b, c = transport_instance(m, n)
         model = Model(solver = GLPKSolverLP(presolve=false))
         @variable(model, x[1:m*n] >= 0)
         @objective(model, Min, dot(x, c))
         @constraint(model, A * x .== b)
-        status = solve(model)
+
+        #benchmarking GLPK
+        benchmarkGLPK  = @benchmark status = solve($model)             samples=10 evals=1
         xj = getvalue(x)
         zj = getobjectivevalue(model)
 
-        # benchmark
-        benchmarkGLPK  = @benchmark status = solve($model)             samples=10 evals=1
-        benchmarkluup = @benchmark simplexluup($c, $A, $b)             samples=10 evals=1
+        #benchmarking simplexluup
+        x = Array{Float64,1}(m*n)
+        benchmarkluup = @benchmark $x .= simplexluup($c, $A, $b, max_iter = 100*$m*$n)[1]             samples=10 evals=1
         #benchmarklu   = @benchmark simplexluup($c, $A, $b, maxups=0)   samples=10 evals=1
 
         println(logfile, "transport problem: $(m+n) rows, $(m*n) cols, $(nnz(A)) NNZ's luup/GLPK: ")
@@ -29,8 +31,7 @@ using Base.Test, BenchmarkTools
         #println(logfile, "  ", judge(median(benchmarkluup), median(benchmarklu)))
 
         # test if simplexluup successfuly solved the benchmark problems
-        x, z, status = simplexluup(c, A, b, max_iter = 100*m*n)
-        @test dot(c, x) ≈ zj atol = 1e-3
+        @test dot(c, x) ≈ zj atol = zj*1e-9
       end
     end
   end
