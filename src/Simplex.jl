eabc3caf06ad9a39a737f99909116c8fec3593c3
RenanOD <renan_oclides@hotmail.com>
2018-12-03 17:34:07 -0200

    separate auxiliar functions from the main function

--- src/Simplex.jl
+++ src/Simplex.jl
@@ -1,5 +1,6 @@
 module Simplex
 using Base.SparseArrays.halfperm!, Base.SparseArrays.increment!, Base.permute!!, Base.ipermute!!
+include("simplexauxiliar.jl")
 include("simplexluup.jl")
 include("simplexinv.jl")
 
