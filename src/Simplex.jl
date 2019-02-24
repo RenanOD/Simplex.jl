3bb83096be33559daa5978ebd9f96c6ac389df4b
RenanOD <renan_oclides@hotmail.com>
2019-02-23 22:09:08 -0300

    initial 1.1 Julia compatibility

--- src/Simplex.jl
+++ src/Simplex.jl
@@ -1,7 +1,8 @@
 module Simplex
-using Base.SparseArrays.halfperm!, Base.SparseArrays.increment!, Base.permute!!, Base.ipermute!!
-include("simplexauxiliar.jl")
-include("simplexluup.jl")
-include("simplexinv.jl")
+  using LinearAlgebra, SparseArrays
+  import SparseArrays.halfperm!, Base.permute!!
+  include("simplexauxiliar.jl")
+  include("simplexluup.jl")
+  #include("simplexinv.jl")
 
 end
