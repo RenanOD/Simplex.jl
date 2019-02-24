c5466cfc21dab4ccabfdcea12c03eb917104eef1
RenanOD <renan_oclides@hotmail.com>
2019-02-23 22:28:28 -0300

    Main.Simplex -> Simplex

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Pkg
 Pkg.add("JuMP")
 Pkg.add("GLPK")
 
-using Main.Simplex, Test, JuMP, GLPK, Random, LinearAlgebra, SparseArrays
+using Simplex, Test, JuMP, GLPK, Random, LinearAlgebra, SparseArrays
 
   include("test_simplexluup.jl")
   #include("test_simplexinv.jl")
