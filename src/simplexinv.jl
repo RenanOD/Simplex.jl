d2fbdab610394090dee6b06bb3322bb1ab951204
RenanOD <renan_oclides@hotmail.com>
2019-03-11 22:16:45 -0300

    update simplexinv to julia 1.0

--- src/simplexinv.jl
+++ src/simplexinv.jl
@@ -1,23 +1,21 @@
 export simplexinv
 
-function simplexinv(c, A, b, IB=0, invB=0; max_iter = 4000)
+function simplexinv(c, A, b, IB=0, invB=0; max_iter = 20000)
   m, n = size(A)
   iter = 0
   if IB == 0 # construct artificial problem
     artificial = true
     signb = sign.(b)
-    A = [A diagm(signb)]
-    Im = speye(m)
+    A = [A sparse(Diagonal(signb))]
     IB = collect(n+1:n+m) # indexes of basic variables
     IN = collect(1:n)
     co = collect(c)
     c = [zeros(n); ones(m)]
-    invB = collect(diagm(signb))
+    invB = sparse(Diagonal(signb))
     r = -A[:,IN]'*signb # artificial relative costs
     xB = collect(abs.(b)) # solution in current basis
   else
     artificial = false
-    Im = speye(m)
     IN = setdiff(1:n, IB)
     r = c[IN] - A[:,IN]' * (invB' * c[IB])
     xB = invB*b
@@ -26,30 +24,32 @@ function simplexinv(c, A, b, IB=0, invB=0; max_iter = 4000)
 
   status = :Optimal
 
-  while !(q == 0 || iter >= max_iter)
+  while !(q == nothing || iter >= max_iter) # relative variable changes to directioner >= max_iter)
     iter += 1
-    @assert all(xB .>= 0)
     d = invB * A[:,IN[q]] # viable direction
-    apfrac = xB ./ d # relative variable changes to direction
-    indpos = find(d .> 0) # variables that decrease in d direction
-    if length(indpos) == 0
-      status = :Unbounded
-      break
-    end
-    indxq = indmin(apfrac[indpos])
-    xq = apfrac[indpos[indxq]]
-    @assert xq >= 0
-    @assert xq < Inf
 
-    p = findfirst(apfrac, xq) # Bland's Rule
+    xq = Inf
+    for k in 1:m # find min xB/d s.t. d .> 0
+      if d[k] >= 2e-16
+        dfrac = xB[k]/d[k]
+        if dfrac < xq
+          xq = dfrac
+          p = k
+        end
+      end
+    end
+    if xq == Inf
+      status = :Unbounded; break
+    end
+
     xB -= xq * d; xB[p] = xq # update solution
     IB[p], IN[q] = IN[q], IB[p] # update indexes
     #update of inverse of B
-    Q = collect(Im)
+    E = one(zeros(m,m))
     dp = d[p]
     d[p] = -1
-    Q[:, p] = -d / dp
-    invB = Q*invB #no need to create Q
+    E[:, p] = -d / dp
+    invB = E*invB #no need to create E
     r = c[IN] - A[:,IN]' * (invB' * c[IB])
     q = findfirst(r .< 0) # Bland's Rule
   end
@@ -65,38 +65,36 @@ function simplexinv(c, A, b, IB=0, invB=0; max_iter = 4000)
   else
     if dot(xB, c[IB]) > 0
       status = :Infeasible
-      I = find(IB .<= n - m)
+      I = findall(IB .<= n - m)
       x[I] = xB[I]
       z = dot(co, x)
     elseif maximum(IB) > n # check for artificial variables in basis
-      deleteat!(IN, find(IN .> n))
+      deleteat!(IN, findall(IN .> n))
       Irows = collect(1:m)
       p = findfirst(IB .> n)
-      while p != 0
+      while p != nothing
         q = findfirst(invB[p,:]' * A[Irows,IN] .!= 0)
-        if q == 0
+        if q == nothing
           deleteat!(Irows, p)
           deleteat!(IB, p)
-          invB = inv(A[Irows,IB])
-          Im = speye(length(Irows))
+          invB = inv(Matrix(A[Irows,IB]))
         else
           IB[p] = IN[q]
           deleteat!(IN, q)
           d = invB * A[Irows,IN[q]]
-          Q = collect(Im)
+          E = one(zeros(m,m))
           dp = d[p]
           d[p] = -1
-          Q[:, p] = -d / dp
+          E[:, p] = -d / dp
           invB = Q*invB
         end
         p = findfirst(IB .> n)
       end
-      @assert length(Irows) > 0
       x, z, status = simplexinv(co, A[Irows,1:n], b[Irows], IB, invB)
     else
       x, z, status = simplexinv(co, A[:,1:n], b, IB, invB)
     end
   end
 
-  return x, z, status, IB
+  return x, z, status
 end
